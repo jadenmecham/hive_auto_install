70cbc3f86a7323e8e289fe037376407111a1ddb6
jared <jaredpaquet@ufl.edu>
2021-03-11 17:44:31 -0600

    Add exfat-utils, and missing yes prompts

--- autoinstall.sh
+++ autoinstall.sh
@@ -24,7 +24,7 @@ gazebo=false
 esac
 
 #Update System
-sudo apt update
+sudo apt update -y
 sudo apt dist-upgrade -y
 
 #Add Repos
@@ -33,7 +33,7 @@ sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
 sudo apt update -y
 
 #Apt install programs
-sudo apt install -y git terminator openssh-server python3-pip net-tools remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice
+sudo apt install -y git terminator exfat-utils openssh-server python3-pip net-tools remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice
 
 echo $ros
 if [[ $(lsb_release -rs) == "20.04" ]]
@@ -44,14 +44,14 @@ then
     then
         sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
         sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-        sudo apt update
-        sudo apt install ros-noetic-desktop-full
+        sudo apt update -y
+        sudo apt install ros-noetic-desktop-full -y
         sudo echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
         source ~/.bashrc
     fi
 elif [[ $(lsb_release -rs) == "18.04" ]]
 then
-    sudo apt install python-pip
+    sudo apt install python-pip -y
     if $ros
     then
         sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -66,7 +66,7 @@ then
     fi
 elif [[ $(lsb_release -rs) == "16.04" ]]
 then
-    sudo apt install python-pip
+    sudo apt install python-pip -y
     if $ros
     then
         sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -78,7 +78,7 @@ then
         echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
         source ~/.bashrc
         sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools -y
-        sudo apt-get install ros-kinetic-turtlebot ros-kinetic-turtlebot-gazebo ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki-ftdi ros-kinetic-ar-track-alvar-msgs
+        sudo apt-get install ros-kinetic-turtlebot ros-kinetic-turtlebot-gazebo ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki-ftdi ros-kinetic-ar-track-alvar-msgs -y
     fi
 else
     echo "Non-compatible version"
