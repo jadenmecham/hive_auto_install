780d229d15e9f484e4072a61f940c466c9ae7e5b
jared <jaredpaquet@ufl.edu>
2021-03-12 11:45:17 -0600

    Update git repo link

--- install_msi_gs75_dependencies.sh
+++ install_msi_gs75_dependencies.sh
@@ -1,5 +1,5 @@
 #Install Keyboard Software
-git clone https://github.com/Askannz/msi-perkeyrgb
+git clone https://github.com/Askannz/msi-perkeyrgb.git
 cd msi-perkeyrgb/
 sudo python3 setup.py install
 sudo cp 99-msi-rgb.rules /etc/udev/rules.d/
