70c2713249742f3288cd4eeca14bb1867ffe1608
jared <jaredpaquet@ufl.edu>
2021-03-19 17:37:03 -0500

    Add adaptive control bundle

--- autoinstall.sh
+++ autoinstall.sh
@@ -12,10 +12,16 @@ case $input in
         exit 1
         ;;
     esac
+    read -r -p "Would you like to install the Adaptive Control Bundle? [Y/n] " input3
+    case $input3 in
+    [yY][eE][sS]|[yY]) adaptive=true;;
+    [nN][oO]|[nN])adaptive=false;;
+    esac
 ;;
 [nN][oO]|[nN])
 ros=false
 gazebo=false
+adaptive=false
 ;;
 *)
     echo "Invalid input..."
@@ -46,6 +52,7 @@ then
         sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
         sudo apt update -y
         sudo apt install ros-noetic-desktop-full -y
+        sudo apt install ros-noetic-joy* ros-noetic-eigen-stl
         sudo echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
         source ~/.bashrc
     fi
@@ -63,6 +70,7 @@ then
         echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
         source ~/.bashrc
         sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+        sudo apt install ros-melodic-joy* ros-melodic-eigen-stl
     fi
 elif [[ $(lsb_release -rs) == "16.04" ]]
 then
@@ -90,6 +98,18 @@ fi
 if $gazebo
 then
     curl -sSL http://get.gazebosim.org | sh
+fi
+if $adaptive
+then
+    mkdir -p adaptive_ws/src
+    cd adaptive_ws/src
+    catkin_init_workspace
+    git clone https://github.com/uf-reef-avl/reef_adaptive_control_bundle
+    cd reef_adaptive_control_bundle
+    git submodule update --init --recursive
+    cd ../..
+    catkin build -j 1
+    source devel/setup.bash
 fi
 #Snap install programs
 sudo snap install pycharm-community --classic
