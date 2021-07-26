27c0b9e42a7935c8e0bab2e88dc745fcc6c55b8a
paul.buzaud <paul.buzaud@ufl.edu>
2021-07-26 14:50:30 -0500

    [FIX] make gtsam c++ executable

--- gtsam_cpp.sh
+++ gtsam_cpp.sh
@@ -5,13 +5,13 @@ git clone https://github.com/borglab/gtsam
 cd gtsam
 git checkout -b 4.0.3
 mkdir build && cd build
-read -p "Do you want to run unit tests? (this may take a while): " var
-var="${var,,}"
+#read -p "Do you want to run unit tests? (this may take a while): " var
+#var="${var,,}"
 cmake ..
-if [[ "$var" == "yes" || "$var" == "y" ]]
-then
+#if [[ "$var" == "yes" || "$var" == "y" ]]
+#then
  make check
-fi
+#fi
 make -j8
 sudo make install
 sudo ldconfig
