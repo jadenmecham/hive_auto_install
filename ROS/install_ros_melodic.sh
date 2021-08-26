1b0e20ed13f4e4b309f62f1eb0134b888c05b731
jared <jaredpaquet@ufl.edu>
2021-08-26 16:15:40 +0000

    Update melodic install script to fix outdated apt-key, and add additional dependencies

--- ROS/install_ros_melodic.sh
+++ ROS/install_ros_melodic.sh
@@ -1,9 +1,10 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update -y
 sudo apt install ros-melodic-desktop-full -y
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential ros-melodic-joy* ros-melodic-eigen*
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential ros-melodic-joy* ros-melodic-eigen*
