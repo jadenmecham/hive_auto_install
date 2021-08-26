a4c8232109168debc9da09eecbfb1abccc415519
jared <jaredpaquet@ufl.edu>
2021-08-26 16:18:58 +0000

    Update install_ros_noetic.sh

--- ROS/install_ros_noetic.sh
+++ ROS/install_ros_noetic.sh
@@ -1,6 +1,10 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update -y
 sudo apt install -y ros-noetic-desktop-full ros-noetic-joy* ros-noetic-eigen*
 sudo echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo rosdep init
+rosdep update
