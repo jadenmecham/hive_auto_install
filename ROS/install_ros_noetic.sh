0ffe3e4969ce347e720667c14da53fa1c1ee87b9
Jared Paquet <jaredpaquet@ufl.edu>
2022-06-21 18:25:03 +0000

    Update install_ros_noetic.sh to remove * from ros-noetic-joy package due to install conflicts

--- ROS/install_ros_noetic.sh
+++ ROS/install_ros_noetic.sh
@@ -2,7 +2,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update -y
-sudo apt install -y ros-noetic-desktop-full ros-noetic-joy* ros-noetic-eigen*
+sudo apt install -y ros-noetic-desktop-full ros-noetic-joy ros-noetic-eigen*
 sudo echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
