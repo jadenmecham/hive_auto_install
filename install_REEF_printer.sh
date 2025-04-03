168118f17c7ed2e9ba161f5110f80d97d2b4c0a9
Jared Paquet <jaredpaquet@ufl.edu>
2025-04-03 14:47:08 -0500

    Update install_REEF_printer.sh

--- install_REEF_printer.sh
+++ install_REEF_printer.sh
@@ -7,6 +7,9 @@ DEVICEURI=smb://$UFUSERNAME:$UFPASSWORD@ad.ufl.edu/TSS-SRV-Print-2.ad.ufl.edu/EN
 echo 'Downloading drivers...'
 wget https://download.support.xerox.com/pub/drivers/VLC7000/drivers/linux/ar/VersaLink_C7000_5.739.0.0_PPD.zip
 unzip -p VersaLink_C7000_5.739.0.0_PPD.zip Linux/English/xrxC7030.ppd >xrxC7030.ppd
+echo 'Installing prerequisites'
+sudo apt install -Y smbclient
+sudo systemctl restart cups
 echo 'Installing printer'
 sudo lpadmin -p REEFxerox -E -v "$DEVICEURI" -i './xrxC7030.ppd'
 echo 'Cleaning up files and enviroment variables'
@@ -18,7 +21,7 @@ rm ./VersaLink_C7000_5.739.0.0_PPD.zip
 echo 'Printing test page'
 lpr -P REEFxerox /usr/share/cups/data/testprint
 sleep 5
-output=lpstat -p REEFxerox
+output=$(lpstat -p REEFxerox)
 if [[ "$output" == "printer REEFxerox is idle."* ]]; then
   echo '🎉Congrats you have successfully added the REEF printer!🎉 Please pickup your test page from the printer in the front office.'
 else
