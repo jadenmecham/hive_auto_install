35b849ad6334b096ada712aeeb527dafc3cfc51b
Jared Paquet <jaredpaquet@ufl.edu>
2025-04-04 11:18:51 -0500

    Update install_REEF_printer.sh

--- install_REEF_printer.sh
+++ install_REEF_printer.sh
@@ -4,8 +4,20 @@ read -p "Enter UF username: " UFUSERNAME
 echo -n "Enter UF password: "
 read -s UFPASSWORD
 echo
+QUEUE=TSS-SRV-Print-2.ad.ufl.edu/ENG-REEF142-PRT-C7030_BW
+DEVICEURI=smb://$UFUSERNAME:$UFPASSWORD@ad.ufl.edu/$QUEUE
+smbclient -L //$QUEUE -U $UFUSERNAME%$UFPASSWORD &>/dev/null
+status=$?
+while [$status -eq 0];
+do
+    echo "Password incorrect"
+    echo -n "Enter UF password: "
+    read -s UFPASSWORD
+    echo
+    smbclient -L //$QUEUE -U $UFUSERNAME%$UFPASSWORD &>/dev/null
+    status=$?
+done
 UFPASSWORD=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]), "\n"' "$UFPASSWORD")
-DEVICEURI=smb://$UFUSERNAME:$UFPASSWORD@ad.ufl.edu/TSS-SRV-Print-2.ad.ufl.edu/ENG-REEF142-PRT-C7030_BW
 echo 'Downloading drivers...'
 wget https://download.support.xerox.com/pub/drivers/VLC7000/drivers/linux/ar/VersaLink_C7000_5.739.0.0_PPD.zip
 unzip -p VersaLink_C7000_5.739.0.0_PPD.zip Linux/English/xrxC7030.ppd >xrxC7030.ppd
