21fba0c70748424d97f8911ef9deec3148a37e39
Jared <jared@jaredframework.pathfinder.reef.ufl.edu>
2025-04-09 16:10:06 -0500

    Add support for 24.04 and bugfixes

--- ROS/install_ros2_jazzy.sh
+++ ROS/install_ros2_jazzy.sh
@@ -7,12 +7,12 @@ sudo apt install -y curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 sudo rm -f /etc/apt/trusted.gpg.d/osrf.gpg
 apt-key export osrf | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/osrf.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
-sudo apt install -y libpython3-dev python3-pip ros-humble-desktop ros-humble-turtlebot3* ros-humble-tf* python3-rosdep python3-colcon-common-extensions ros-humble-ros-gz
+sudo apt install -y libpython3-dev python3-pip ros-jazzy-desktop ros-jazzy-turtlebot3* ros-jazzy-tf* ros-dev-tools python3-rosdep python3-colcon-common-extensions ros-jazzy-ros-gz
 sudo pip3 install transforms3d
 sudo pip3 install -U argcomplete
-grep -q -F 'source /opt/ros/humble/setup.bash' ~/.bashrc || echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+grep -q -F 'source /opt/ros/jazzy/setup.bash' ~/.bashrc || echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo rosdep init
 rosdep update
--- autoinstall.sh
+++ autoinstall.sh
@@ -21,18 +21,6 @@ if [ $major -ge 20 ];
 	else
 		ros2=false
 	fi
-if $ros || $ros2
-    then
-        read -p "Would you like to install Gazebo? [Y/n]: " yn
-        case $yn in
-            [Yy]* ) gazebo=true;;
-            [Nn]* ) gazebo=false;;
-            "" ) gazebo=true;;
-            * ) gazebo=false;;
-        esac
-    else
-        gazebo=false
-    fi
 
 #Disable ipv6
 sh ./disable_ipv6.sh
@@ -49,22 +37,32 @@ sudo apt-key export D38B4796 | sudo gpg --dearmor -o /usr/share/keyrings/google.
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 sudo add-apt-repository -y ppa:graphics-drivers
 sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
+sudo apt install -y curl
 curl -fsS -o- https://deb.packages.mattermost.com/setup-repo.sh | sudo bash
 sudo curl -sSfL https://packages.openvpn.net/packages-repo.gpg >/etc/apt/keyrings/openvpn.asc
 sudo apt update -y
 
 #Apt install programs
-sudo apt install -y git terminator openssh-server python3-pip net-tools remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice libgmock-dev google-chrome-stable apt-transport-https \
-    curl gnome-shell-extension-manager nm-connection-editor network-manager-openconnect-gnome mattermost-desktop 
+sudo apt install -y git terminator openssh-server python3-pip net-tools remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice libgmock-dev google-chrome-stable apt-transport-https gnome-shell-extension-manager network-manager-openconnect-gnome mattermost-desktop
 
-if [[ $os_release == "22.04" ]]; then
+if [[ $os_release == "24.04" ]]; then
 	if $ros
         then
             sh ./ROS/install_ros_one.sh
-    fi
+        fi
 	if $ros2
-		then
-			sh ./ROS/install_ros2_humble.sh
+	then
+	    sh ./ROS/install_ros2_jazzy.sh
+	fi
+elif [[ $os_release == "22.04" ]]; then
+	sudo apt install -y nm-connection-editor
+	if $ros
+        then
+            sh ./ROS/install_ros_one.sh
+        fi
+	if $ros2
+	then
+	    sh ./ROS/install_ros2_humble.sh
 	fi
 elif [[ $os_release == "20.04" ]]; then
 	sh ./install_2004_utils.sh
@@ -73,39 +71,35 @@ elif [[ $os_release == "20.04" ]]; then
         then
             sh ./ROS/install_ros_noetic.sh
             sudo apt install -y python3-catkin*
-    fi
+        fi
 	if $ros2
-		then
-			sh ./ROS/install_ros2_foxy.sh
+	then
+	    sh ./ROS/install_ros2_foxy.sh
 	fi
 elif [[ $os_release == "18.04" ]]; then
 	sudo apt install -y exfat-utils
 	sudo apt install python-pip -y
 	if $ros
 	then
-		sh ./ROS/install_ros_melodic.sh
-		sudo apt install -y python-catkin-tools
+	    sh ./ROS/install_ros_melodic.sh
+	    sudo apt install -y python-catkin-tools
 	fi
 elif [[ $os_release == "16.04" ]]; then
 	sudo apt install -y exfat-utils
 	sudo apt install python-pip -y
 	if $ros
 	then
-		sh ./ROS/install_ros_kinetic.sh
-		sudo apt install -y python-catkin-tools
+	    sh ./ROS/install_ros_kinetic.sh
+	    sudo apt install -y python-catkin-tools
 	fi
 else
 	echo "Non-compatible version"
 fi
-if $gazebo
-then
-	curl -sSL http://get.gazebosim.org | sh
-fi
 
 #Apt install deb files
-wget https://zoom.us/client/6.2.11.5069/zoom_amd64.deb
-sudo apt install -y ./zoom_amd64.deb
-
+arch=$(uname -m | sed 's/x86_64/amd64/')
+wget "https://zoom.us/client/latest/zoom_$arch.deb"
+sudo apt install -y "./zoom_$arch.deb"
 
 #Snap install programs
 sudo snap install pycharm-community --classic
